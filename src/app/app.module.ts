1db7471b5af03222d1a137035b9c2757de8042b9
trbenne2 <luvsrub@Tylers-MacBook-Pro-2.local>
2020-03-01 18:34:05 -0500

    Some frontend work with commonents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+
+// Recipe components
+import { RecipeComponent } from "./recipe/recipe.component";
+import { RecipeListComponent } from "./recipe/recipe-list/recipe-list.component";
+import { RecipeDetailComponent } from "./recipe/recipe-detail/recipe-detail.component";
+import { RecipeItemComponent } from "./recipe/recipe-list/recipe-item/recipe-item.component";
+
+// Shopping componets
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeItemComponent,
+    RecipeComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    ShoppingEditComponent,
+    ShoppingListComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
